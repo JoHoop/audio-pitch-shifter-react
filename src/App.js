5643248d59529c05419f81f8c36964891afc0a55
JoHoop <jolienhoop@gmail.com>
2021-09-18 13:32:31 +0200

    Add vertical spacing below header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Header } from './components/Header';
 import { AudioWrapper } from './components/AudioWrapper';
 import { ThemeWrapper } from './theme/ThemeProvider';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Box } from '@mui/material';
 
 export const App = () => {
   return (
     <ThemeWrapper>
       <CssBaseline />
-      <Header />
-      <AudioWrapper />
+      <Box flexDirection='column'>
+        <Header />
+        <Box mt={8}>
+          <AudioWrapper />
+        </Box>
+      </Box>
     </ThemeWrapper>
   );
 };
